441651e7d38f5ee2a6a6feaba91a2e3135e28de2
kift <su.shihjie@gmail.com>
2013-09-26 10:03:36 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twoorigin::Application.config.secret_token = '8c98999cbab1439e289d8915961e65573eb9c6eb1e89d371855e8907241b8effea62c74c46507fef7b6649ffb69d03bbe21e4603ee510c6a96d1ab7d712387d7'
